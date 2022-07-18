f71ed5d440b2e7ee6c19dca65c2c53b83b633775
daoleno <daodaoonline@gmail.com>
2022-07-19 00:22:44 +0800

    frontend starter kit

---
+++ flow-nft-starter/src/cadence/scripts/getTotalSupply_script.js
@@ -0,0 +1,11 @@
+export const getTotalSupply =
+  // REPLACE THIS WITH YOUR CONTRACT NAME + ADDRESS
+  `
+import BottomShot from 0xd9f8f1715b11f262;
+
+pub fun main(): UInt64 {
+
+    return BottomShot.totalSupply;
+
+}
+`;
